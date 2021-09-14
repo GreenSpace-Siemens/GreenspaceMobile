c9fdd8cd612288364bafa0f3eae0c05a7ed87d70
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-09-14 10:19:58 -0400

    Icons are in

--- components/dashboard/Dashboard.js
+++ components/dashboard/Dashboard.js
@@ -2,9 +2,14 @@ import React from 'react';
 import { View, StyleSheet, Text, Button } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Entypo from 'react-native-vector-icons/Entypo';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons';
 
 const Stack = createNativeStackNavigator();
 
+// Stack Navigators will route to other pages or components
 function Home() {
     return (
         <View>
@@ -45,10 +50,58 @@ function Dashboard({ navigation, route }) {
     return (
         <View style={{ height: '100%' }}>
             <Tab.Navigator initialRouteName="Home">
-                <Tab.Screen name="Home" component={Home} />
-                <Tab.Screen name="Messages" component={Messages} />
-                <Tab.Screen name="SavedJobs" component={SavedJobs} />
-                <Tab.Screen name="Profile" component={Profile} />
+                <Tab.Screen
+                    name="Home"
+                    component={Home}
+                    options={{
+                        tabBarLabel: 'Home',
+                        tabBarIcon: () => (
+                            <Entypo name="home" color={'#0FA97D'} size={28} />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Messages"
+                    component={Messages}
+                    options={{
+                        tabBarLabel: 'Messages',
+                        tabBarIcon: () => (
+                            <Entypo
+                                name="message"
+                                color={'#0FA97D'}
+                                size={28}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="SavedJobs"
+                    component={SavedJobs}
+                    options={{
+                        tabBarLabel: 'Saved',
+                        tabBarIcon: () => (
+                            <FontAwesome
+                                name="star"
+                                color={'#0FA97D'}
+                                size={28}
+                            />
+                        ),
+                    }}
+                />
+                <Tab.Screen
+                    name="Profile"
+                    component={Profile}
+                    options={{
+                        tabBarLabel: 'Saved',
+                        tabBarIcon: () => (
+                            <Ionicons
+                                name="md-person-circle-sharp"
+                                color={'#0FA97D'}
+                                size={30}
+                            />
+                        ),
+                    }}
+                />
             </Tab.Navigator>
         </View>
     );
