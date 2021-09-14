62c805f9fcacbfe5213c93afad08d9be7119ae57
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-09-14 08:25:49 -0400

    Emai and Password inputs can be passed to Dashboard

--- components/dashboard/Dashboard.js
+++ components/dashboard/Dashboard.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, StyleSheet, Text, Button } from 'react-native';
 
-function Dashboard({ navigation }) {
-  return (
-    <View>
-      <Text>Dashboard</Text>
-    </View>
-  );
+function Dashboard({ navigation, route }) {
+    const { username, password } = route.params;
+
+    return (
+        <View>
+            <Text>Dashboard</Text>
+            <Text>{`Username: ${username}`}</Text>
+            <Text>{`Password: ${password}`}</Text>
+            <Button
+                title="Log Out"
+                onPress={() => navigation.navigate('Login')}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({});
--- components/login/Login.js
+++ components/login/Login.js
@@ -3,106 +3,109 @@ import { View, StyleSheet, Text, TextInput } from 'react-native';
 import { Button } from 'react-native-elements';
 
 function Login({ navigation }) {
-  const [username, setUsername] = React.useState(null);
-  const handleUsername = event => {
-    let username = event.target.value;
-    setUsername(username);
-  };
-
-  const [password, setPassword] = React.useState(null);
-  const handlePassword = event => {
-    let password = event.target.value;
-    setPassword(password);
-  };
-
-  const handleLogin = event => {
-    const user = {
-      username: username,
-      password: password,
+    const [username, setUsername] = React.useState(null);
+    const handleUsername = username => {
+        setUsername(username);
     };
 
-    console.log(`Username: ${username}`);
-    console.log(`Password: ${password}`);
+    const [password, setPassword] = React.useState(null);
+    const handlePassword = password => {
+        setPassword(password);
+    };
 
-    navigation.navigate('Dashboard');
-  };
+    const handleLogin = () => {
+        const user = {
+            username: username,
+            password: password,
+        };
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Text style={styles.title}>GREENSPACE</Text>
-      </View>
-      <View style={styles.loginForm}>
-        <TextInput
-          placeholder="Email"
-          style={styles.input}
-          placeholderTextColor="#8D8D8D"
-          onChange={event => handleUsername(event)}
-        />
-        <TextInput
-          placeholder="Password"
-          style={styles.input}
-          placeholderTextColor="#8D8D8D"
-          onChange={event => handlePassword(event)}
-        />
-        <Text style={styles.link}>Forgot Password?</Text>
-        <Button
-          title="Sign In"
-          titleStyle={styles.buttonTitle}
-          containerStyle={styles.buttonContainer}
-          buttonStyle={styles.button}
-          onPress={event => handleLogin(event)}
-        />
-        <Text
-          onPress={() => navigation.navigate('Register')}
-          style={styles.link}>
-          Create Account?
-        </Text>
-      </View>
-    </View>
-  );
+        console.log(`Username: ${username}`);
+        console.log(`Password: ${password}`);
+
+        navigation.navigate('Dashboard', user);
+
+        setUsername(null);
+        setPassword(null);
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Text style={styles.title}>GREENSPACE</Text>
+            </View>
+            <View style={styles.loginForm}>
+                <TextInput
+                    placeholder="Email"
+                    style={styles.input}
+                    placeholderTextColor="#8D8D8D"
+                    onChangeText={username => handleUsername(username)}
+                    value={username}
+                />
+                <TextInput
+                    placeholder="Password"
+                    style={styles.input}
+                    placeholderTextColor="#8D8D8D"
+                    onChangeText={password => handlePassword(password)}
+                    value={password}
+                />
+                <Text style={styles.link}>Forgot Password?</Text>
+                <Button
+                    title="Sign In"
+                    titleStyle={styles.buttonTitle}
+                    containerStyle={styles.buttonContainer}
+                    buttonStyle={styles.button}
+                    onPress={() => handleLogin()}
+                />
+                <Text
+                    onPress={() => navigation.navigate('Register')}
+                    style={styles.link}>
+                    Create Account?
+                </Text>
+            </View>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    height: '100%',
-    backgroundColor: '#ffffff',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    paddingTop: 130,
-    paddingBottom: 200,
-    paddingLeft: 40,
-    paddingRight: 40,
-  },
-  header: {
-    backgroundColor: 'white',
-  },
-  title: {
-    textAlign: 'center',
-    fontSize: 40,
-    fontWeight: '800',
-    color: '#0FA97D',
-  },
-  loginForm: {},
-  input: {
-    backgroundColor: '#EFEFEF',
-    fontSize: 20,
-    borderRadius: 8,
-    elevation: 3,
-    marginBottom: 15,
-    padding: 10,
-  },
-  link: { color: '#000000', textAlign: 'center' },
-  buttonContainer: {
-    borderRadius: 8,
-    marginTop: 10,
-    marginBottom: 10,
-  },
-  button: {
-    backgroundColor: '#0FA97D',
-  },
-  buttonTitle: { fontSize: 17 },
+    container: {
+        height: '100%',
+        backgroundColor: '#ffffff',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        paddingTop: 130,
+        paddingBottom: 200,
+        paddingLeft: 40,
+        paddingRight: 40,
+    },
+    header: {
+        backgroundColor: 'white',
+    },
+    title: {
+        textAlign: 'center',
+        fontSize: 40,
+        fontWeight: '800',
+        color: '#0FA97D',
+    },
+    loginForm: {},
+    input: {
+        backgroundColor: '#EFEFEF',
+        fontSize: 20,
+        borderRadius: 8,
+        elevation: 3,
+        marginBottom: 15,
+        padding: 10,
+    },
+    link: { color: '#000000', textAlign: 'center' },
+    buttonContainer: {
+        borderRadius: 8,
+        marginTop: 10,
+        marginBottom: 10,
+    },
+    button: {
+        backgroundColor: '#0FA97D',
+    },
+    buttonTitle: { fontSize: 17 },
 });
 
 export default Login;
