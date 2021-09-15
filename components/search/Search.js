e405278dd131fc4f1139c35f44bc6075550479a1
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-09-15 15:42:55 -0400

    Getting search started

--- components/search/Search.js
+++ components/search/Search.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import { View, StyleSheet, Text, Button } from 'react-native';
+import { View, StyleSheet, TextInput, Button } from 'react-native';
 
 function Search() {
     return (
-        <View style={{ height: '100%', backgroundColor: '#ffffff' }}>
-            <Text>Search</Text>
+        <View style={styles.container}>
+            <View style={styles.searchBar}>
+                <TextInput />
+            </View>
+            <View style={styles.resultsContainer}></View>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: { height: '100%', backgroundColor: '#ffffff' },
+    searchBar: { flex: 1, borderBottomWidth: 0.2, borderColor: '#525B76' },
+    resultsContainer: { flex: 8 },
+});
 
 export default Search;
