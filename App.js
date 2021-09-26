81a8fa3c3a1b5ac26d259bfdb678d213930d3f75
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-09-26 13:53:29 -0400

    Refactor Overhaul

--- App.js
+++ App.js
@@ -3,17 +3,40 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
-// Child components
+// * Components
+import { Cover, About, Experiences, Education } from './components/forms/Forms';
+import Applied from './components/applied/Applied';
+import SavedJobs from './components/savedjobs/SavedJobs';
+
+// * Scenes
 import Login from './components/login/Login';
 import Register from './components/register/Register';
+import Main from './components/main/Main'; // ! Home
+import Notifications from './components/notifications/Notifications';
+import Recipients from './components/recipients/Recipients'; // ! Messages
+import ProfilePage from './components/profilepage/ProfilePage';
+import { NotificationsSettings } from './components/settings/Settings';
+import { Subscription } from './components/settings/Settings';
+import { Settings } from './components/settings/Settings';
+import { Account } from './components/account/Account';
+import {
+    ChangeEmail,
+    ChangePhone,
+    ChangePassword,
+} from './components/forms/Forms';
+
+// * Navigations
 import Dashboard from './components/dashboard/Dashboard';
+
+// * Not Sure
 
 const Stack = createNativeStackNavigator();
 
 function App() {
     return (
         <SafeAreaProvider>
-            <NavigationContainer>
+            <Account />
+            {/* <NavigationContainer>
                 <Stack.Navigator initialRouteName="Login">
                     <Stack.Screen
                         name="Login"
@@ -31,7 +54,7 @@ function App() {
                         options={{ headerShown: false }}
                     />
                 </Stack.Navigator>
-            </NavigationContainer>
+            </NavigationContainer> */}
         </SafeAreaProvider>
     );
 }
