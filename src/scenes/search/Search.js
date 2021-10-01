6d0f113ddae709135175b20cd0d8bf3645b32e45
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-10-01 15:27:11 -0400

    Changed to nativebase

--- src/scenes/search/Search.js
+++ src/scenes/search/Search.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, TextInput } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Input } from 'native-base';
 import { Colors } from '../../styles/index';
 
