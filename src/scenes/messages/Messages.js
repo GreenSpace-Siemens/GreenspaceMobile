3c4d03a46abc2220ab192f1306eec3aa634d16ca
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-10-13 14:08:27 -0400

    Added list to messages

--- src/scenes/messages/Messages.js
+++ src/scenes/messages/Messages.js
@@ -3,6 +3,7 @@ import { View, StyleSheet, Text } from 'react-native';
 import Header from '../../components/organisms/header/Header';
 import { Colors } from '../../styles/index';
 import CustomModal from '../../components/molecules/custommodal/CustomModal';
+import SwipeList from '../../components/molecules/swipelist/SwipeList';
 
 function Messages({ navigation }) {
     const [open, setOpen] = React.useState(false);
@@ -10,6 +11,69 @@ function Messages({ navigation }) {
     const toggleModal = () => {
         setOpen(!open);
     };
+
+    const data = [
+        {
+            id: 1,
+            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            header: 'Header Name',
+            subheader: 'This is a subheader',
+        },
+        {
+            id: 2,
+            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            header: 'Header Name',
+            subheader: 'This is a subheader',
+        },
+        {
+            id: 3,
+            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            header: 'Header Name',
+            subheader: 'This is a subheader',
+        },
+        {
+            id: 4,
+            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            header: 'Header Name',
+            subheader: 'This is a subheader',
+        },
+        {
+            id: 5,
+            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            header: 'Header Name',
+            subheader: 'This is a subheader',
+        },
+        {
+            id: 6,
+            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            header: 'Header Name',
+            subheader: 'This is a subheader',
+        },
+        {
+            id: 7,
+            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            header: 'Header Name',
+            subheader: 'This is a subheader',
+        },
+        {
+            id: 8,
+            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            header: 'Header Name',
+            subheader: 'This is a subheader',
+        },
+        {
+            id: 9,
+            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            header: 'Header Name',
+            subheader: 'This is a subheader',
+        },
+        {
+            id: 10,
+            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
+            header: 'Header Name',
+            subheader: 'This is a subheader',
+        },
+    ];
 
     return (
         <View style={styles.container}>
@@ -21,7 +85,7 @@ function Messages({ navigation }) {
                 toggleModal={toggleModal}
             />
             <View style={styles.body}>
-                <Text>Recipents</Text>
+                <SwipeList data={data} />
             </View>
             <CustomModal
                 open={open}
@@ -38,7 +102,12 @@ function Messages({ navigation }) {
 
 const styles = StyleSheet.create({
     container: { height: '100%', backgroundColor: Colors.WHITE },
-    body: { flex: 9, borderTopWidth: 0.5, borderTopColor: Colors.GRAY_DARK },
+    body: {
+        flex: 9,
+        flexGrow: 9,
+        borderTopWidth: 0.5,
+        borderTopColor: Colors.GRAY_DARK,
+    },
 });
 
 export default Messages;
