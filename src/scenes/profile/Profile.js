78f2591e49ac9ad77f6ed29a33bd87d52d5413ce
Nolan Donley <nolandonley14@gmail.com>
2021-10-15 13:49:50 -0400

    added firebase functionality

--- src/scenes/profile/Profile.js
+++ src/scenes/profile/Profile.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { View, StyleSheet, Text } from 'react-native';
+import { Button, Input } from 'native-base';
 import Header from '../../components/organisms/header/Header';
 import { Colors } from '../../styles/index';
+import auth from '@react-native-firebase/auth';
 
 function Profile({ navigation }) {
+
+    const handleSignOut = () => {
+      auth().signOut().then(() => {
+        navigation.navigate('Login');
+        console.log('User signed out!')
+      })
+      .catch(error => {
+        console.error(error);
+      });
+    }
+
     return (
         <View style={styles.container}>
             <Header
@@ -14,6 +27,9 @@ function Profile({ navigation }) {
             />
             <View style={styles.body}>
                 <Text>Body</Text>
+                <Button style={styles.button} onPress={() => handleSignOut()}>
+                    Sign out
+                </Button>
             </View>
         </View>
     );
