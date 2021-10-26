1ae0a5e7bbc415a64c8b2711df7e15ce09337dd2
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-10-26 11:54:06 -0400

    Notifications banner

--- src/scenes/notifications/Notifications.js
+++ src/scenes/notifications/Notifications.js
@@ -7,69 +7,6 @@ import Header from '../../components/organisms/header/Header';
 import SwipeList from '../../components/molecules/swipelist/SwipeList';
 
 function Notifications({ navigation }) {
-    const data = [
-        {
-            id: 1,
-            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-            header: 'Header Name',
-            subheader: 'This is a subheader',
-        },
-        {
-            id: 2,
-            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-            header: 'Header Name',
-            subheader: 'This is a subheader',
-        },
-        {
-            id: 3,
-            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-            header: 'Header Name',
-            subheader: 'This is a subheader',
-        },
-        {
-            id: 4,
-            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-            header: 'Header Name',
-            subheader: 'This is a subheader',
-        },
-        {
-            id: 5,
-            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-            header: 'Header Name',
-            subheader: 'This is a subheader',
-        },
-        {
-            id: 6,
-            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-            header: 'Header Name',
-            subheader: 'This is a subheader',
-        },
-        {
-            id: 7,
-            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-            header: 'Header Name',
-            subheader: 'This is a subheader',
-        },
-        {
-            id: 8,
-            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-            header: 'Header Name',
-            subheader: 'This is a subheader',
-        },
-        {
-            id: 9,
-            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-            header: 'Header Name',
-            subheader: 'This is a subheader',
-        },
-        {
-            id: 10,
-            avatar: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-            header: 'Header Name',
-            subheader: 'This is a subheader',
-        },
-    ];
-
     return (
         <View style={styles.container}>
             <Header
@@ -80,7 +17,7 @@ function Notifications({ navigation }) {
             />
 
             <View style={styles.body}>
-                <SwipeList data={data} />
+                <Text style={styles.text}>Notifications Coming Soon</Text>
             </View>
         </View>
     );
@@ -94,6 +31,13 @@ const styles = StyleSheet.create({
         flexGrow: 9,
         borderTopWidth: 0.5,
         borderTopColor: Colors.GRAY_DARK,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    text: {
+        color: Colors.GREEN,
+        fontSize: 20,
+        fontWeight: 'bold',
     },
 });
 
