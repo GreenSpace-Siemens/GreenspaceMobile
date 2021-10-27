568a9a8887f839661a62f15584449570487dd656
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-10-27 12:46:43 -0400

    Filled Add Skill page with skils

--- src/components/organisms/background/Background.js
+++ src/components/organisms/background/Background.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, ScrollView, StyleSheet, Text } from 'react-native';
 import { Colors } from '../../../styles/index';
 import CustomModal from '../../molecules/custommodal/CustomModal';
+
+import Skill from '../../atoms/skill/Skill';
+
+import { profile } from '../../../database/Database';
 
 function Background({ navigation, route }) {
     const { link } = route.params;
@@ -14,9 +18,22 @@ function Background({ navigation, route }) {
 
     return (
         <View style={styles.container}>
-            <Text style={styles.link} onPress={() => toggleModal()}>
-                {link}
-            </Text>
+            <ScrollView style={styles.scrollContainer}>
+                {profile.skills.map((skill, i) => {
+                    return (
+                        <Skill
+                            key={i}
+                            islast={
+                                i + 1 >= profile.skills.length ? true : false
+                            }
+                            skill={skill}
+                        />
+                    );
+                })}
+                <Text style={styles.link} onPress={() => toggleModal()}>
+                    {link}
+                </Text>
+            </ScrollView>
             <CustomModal
                 open={open}
                 toggleModal={toggleModal}
@@ -31,8 +48,14 @@ function Background({ navigation, route }) {
 }
 
 const styles = StyleSheet.create({
-    container: { height: '100%', backgroundColor: Colors.WHITE, padding: 20 },
-    link: { color: Colors.GREEN, fontWeight: 'bold', fontSize: 18 },
+    container: { height: '100%', backgroundColor: Colors.WHITE },
+    scrollContainer: { paddingLeft: 20, paddingRight: 20 },
+    link: {
+        color: Colors.GREEN,
+        fontWeight: 'bold',
+        fontSize: 18,
+        marginTop: 10,
+    },
 });
 
 export default Background;
