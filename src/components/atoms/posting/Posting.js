79d7db6dfe32b800a4ef72ed33e62db58e6b7853
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-11-15 11:55:58 -0500

    Working on customer side

---
+++ src/components/atoms/posting/Posting.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+function Posting({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text>Job Listing</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {},
+});
+
+export default Posting;
---
+++ src/scenes/postings/Postings.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+function Postings({ navigation }) {
+    return (
+        <View style={styles.container}>
+            <Text>Job Listings</Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {},
+});
+
+export default Postings;
