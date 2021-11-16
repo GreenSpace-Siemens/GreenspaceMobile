462b0f57b91528c63116dd601e3e5b26ca61bbf7
stefanwerleman <stefanwerleman@knights.ucf.edu>
2021-11-16 15:42:09 -0500

    Still working on analytics page

---
+++ src/components/atoms/menucard/MenuCard.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { StyleSheet, View, Text, Pressable } from 'react-native';
+import { style } from 'styled-system';
+import { Colors } from '../../../styles/index';
+
+function MenuCard({ navigation, header, value }) {
+    const renderValue = () => {
+        if (value > 999999) {
+            return `${Math.floor(value / 1000000)}M`;
+        } else if (value > 999) {
+            return `${Math.floor(value / 1000)}k`;
+        } else {
+            return value;
+        }
+    };
+
+    return (
+        <Pressable onPress={() => console.log('Go to Active Jobs')}>
+            <View style={styles.container}>
+                <Text
+                    style={[styles.text, { textAlign: 'right', fontSize: 46 }]}>
+                    {renderValue()}
+                </Text>
+
+                <Text
+                    style={[
+                        styles.text,
+                        { textAlign: 'center', fontSize: 20 },
+                    ]}>
+                    {header}
+                </Text>
+            </View>
+        </Pressable>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        height: 140,
+        width: 140,
+        borderRadius: 15,
+        backgroundColor: Colors.BLUE_GREEN,
+        paddingTop: 6,
+        paddingBottom: 10,
+        paddingLeft: 10,
+        paddingRight: 10,
+        justifyContent: 'space-between',
+    },
+    text: { color: Colors.WHITE, fontWeight: 'bold' },
+});
+
+export default MenuCard;
--- src/scenes/login/Login.js
+++ src/scenes/login/Login.js
@@ -63,7 +63,10 @@ function Login({ navigation }) {
                     Alert.alert('The password is invalid.');
                 }
 
-                if (error.code === 'auth/invalid-email') {
+                if (
+                    error.code === 'auth/invalid-email' ||
+                    error.code === 'auth/user-not-found'
+                ) {
                     console.log('That email address is invalid!');
                     Alert.alert('The email is invalid.');
                 }
