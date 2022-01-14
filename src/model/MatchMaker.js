17c7f0ee2949054e4a1925251e283428ba073fce
stefanwerleman <stefanwerleman@knights.ucf.edu>
2022-01-13 19:21:20 -0500

    Switching the ml5.js ML library for KNN classifier

--- src/model/MatchMaker.js
+++ src/model/MatchMaker.js
@@ -4,26 +4,54 @@ class MatchMaker {
 	#data;
 
  	constructor(jobs) {
+		// Waiting for TensorFlow to be ready.
 		this.onWaitReady();
 
+		// Retrieved jobs from database
 		console.log("=====Assessing Jobs:=====");
 		console.log(jobs);
 		console.log();
 
+		// Data Preprocessing
 		this.#data = this.processData(jobs);
 
- 		console.log("=====Created MatchMaker Object=====");
+ 		console.log("=====Data Processed=====");
 		console.log(this.#data);
+		
+		// Create model
+		
  	}
 
 	async onWaitReady() {
 		await tf.ready();
 	}
 
-	processData(jobs) {
-		let data = [1, 2, 3];
+	transform(skills, map, val)
+	{
+		const res = [];
 
-		return tf.tensor(data);
+		for (let skill of skills) {
+			let s = skill.toLowerCase();
+			
+			if (!map.has(s)) {
+				map.set(s, val++)
+			}
+
+			res.push(map.get(s));
+		};
+
+		return res;
+	}
+
+	processData(jobs) {
+		let data = [];
+
+		let transformedData = new Map();
+		jobs.forEach((job) => {
+			data.push(this.transform(job['description']['Skill Requirements'], transformedData, transformedData.size));
+		});
+
+		return data;
 	}
 }
 
